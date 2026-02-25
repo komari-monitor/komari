aaa2402ee2de8686f87555c832cdf0f0b0ee2db2
lyimoexiao <lyimoexiao@outlook.com>
2026-02-26 03:44:26 +0800

    lint: gofmt

--- cmd/flags/config.go
+++ cmd/flags/config.go
@@ -2,13 +2,15 @@ package flags
 
 var (
 	// 数据库配置
-	DatabaseType string // 数据库类型：sqlite, mysql
-	DatabaseFile string // SQLite数据库文件路径
-	DatabaseHost string // MySQL/其他数据库主机地址
-	DatabasePort string // MySQL/其他数据库端口
-	DatabaseUser string // MySQL/其他数据库用户名
-	DatabasePass string // MySQL/其他数据库密码
-	DatabaseName string // MySQL/其他数据库名称
+	DatabaseType     string // 数据库类型：sqlite, postgres
+	DatabaseFile     string // SQLite数据库文件路径
+	DatabaseHost     string // PostgreSQL数据库主机地址
+	DatabasePort     string // PostgreSQL数据库端口
+	DatabaseUser     string // PostgreSQL数据库用户名
+	DatabasePass     string // PostgreSQL数据库密码
+	DatabaseName     string // PostgreSQL数据库名称
+	DatabaseSSLMode  string // PostgreSQL SSL模式 (disable, require, verify-ca, verify-full)
+	DatabaseTimezone string // PostgreSQL 时区设置
 
 	Listen string
 )
